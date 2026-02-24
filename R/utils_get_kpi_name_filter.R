1ab0414472d86b5a67ff6493785bcf7a88993b73
bjdugan <ardentArmadillo24@proton.me>
2026-02-24 16:22:46 -0500

    Added utils, including helper fun for populating UI elements from DB.

---
+++ R/utils_get_kpi_name_filter.R
@@ -0,0 +1,20 @@
+get_kpi_name_filter <- function() {
+  #' get_kpi_name_filter
+  #'
+  #' @description For populating selectInput list with ~static~ content
+  #'
+  #' @return kpi_name
+  #'
+  #' @noRd
+
+  # db connection (added manually; contents of inst should move to root after isntallation)
+  db_path <- system.file("app", "db", "instruments.db", package = "nsseShiny")
+  instruments <- DBI::dbConnect(RSQLite::SQLite(), # db_path)
+                           dbname = "./inst/app/db/instruments.db") # pre-install/dev
+  on.exit(DBI::dbDisconnect(instruments))
+
+  x <- c("None", pull(distinct(tbl(instruments, "kpi_desc"), kpi_name)))
+
+  return(x[!is.na(x)])
+
+}
