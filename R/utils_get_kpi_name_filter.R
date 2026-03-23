da5c8f429de2c39a053dc2432eb8d8a3cb529461
bjdugan <ardentArmadillo24@proton.me>
2026-03-23 14:58:00 -0400

    omitted development notes

--- R/utils_get_kpi_name_filter.R
+++ R/utils_get_kpi_name_filter.R
@@ -13,10 +13,31 @@ get_kpi_name_filter <- function() {
                            dbname = "./inst/app/db/instruments.db") # pre-install/dev
   on.exit(DBI::dbDisconnect(instruments))
 
-  #x <- c("None", pull(distinct(tbl(instruments, "kpi_desc"), kpi_name)))
-  # setting default to 1st, not "None"
+  # for flat select input list, return vector
   x <- pull(distinct(tbl(instruments, "kpi_desc"), kpi_name))
 
+  # # for nested list, return list: list(l1 = c(l2, l2), l1 = c(l2, l2))
+  # left_join(
+  #   tbl(instruments, "kpi_desc"),
+  #   tbl(instruments, "instrument_names"),
+  #   by = "instrument") |>
+  #   left_join(tbl(instruments, "instrument_tracking"), by = "instrument") |>
+  #   distinct(kpi_name, .keep_all = TRUE) |>
+  #   collect() |>
+  #   # factor(instrument) so that order is core, con, mod
+  #   mutate(
+  #     type = factor(type, levels = c("core", "mod", "con")),
+  #     # have to adjust since we kept name but changed abb...fix? Where?
+  #     instrument_name = if_else(instrument == "ADV",
+  #                               "Academic Adivsing (old)",
+  #                               instrument_name)) |>
+  #   arrange(type, instrument) |>
+  #   mutate(instrument_name = forcats::as_factor(instrument_name)) |>
+  #   pull(instrument_name)
+  #   split(~instrument) |>
+  #   lapply(pull, kpi_name)
   return(x[!is.na(x)])
 
 }
+
+
